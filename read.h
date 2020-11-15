c82f16fc7baab545a6f8de3da6eb78f484261357
maqalaqil <maheralaqil1993@gmail.com>
2020-11-15 23:24:06 +0300

    added function to check end of lines

---
+++ read.h
@@ -0,0 +1,33 @@
+
+//read.h - a tiny header-only library to allow input in C++ to be more sensible
+//by maqalaqil
+#ifndef __READ_maqalaqil_H
+#define __READ_maqalaqil_H
+#include <iostream>
+#include <fstream>
+#include <string>
+//Read a T from cin, reprompting if they type in something wrong
+//Examples:
+//int x = read<int>();
+//const int x = read<int>("Please enter an int");
+//string s = read<string>("What is your name?");
+template<class T>
+T read(const std::string prompt = "") {
+	while (true) {
+		if (std::cin.eof()) //We reached the end of file, or the user hit ctrl-d
+			return T(); //Alternatively, we could throw an exception
+		T retval;
+		std::istream::pos_type pos = std::cin.tellg();
+		std::cout << prompt;
+		std::cin >> retval;
+		if (!std::cin) {
+			std::cin.clear(); //Clear error code
+			if(std::cin.tellg() > pos) // check whether the stream has advanced
+				continue;
+			std::string s;
+			std::cin >> s; //Remove the word that caused the error
+			continue;
+		}
+		return retval;
+	}
+}
